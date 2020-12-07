88f28873c2edcfcbde0610971edadf164596d397
Maxim Hait <maxhait25@gmail.com>
2020-12-07 14:54:14 +0200

    spinner

--- app/src/main/java/com/example/fitmeal/RecommendMeals.java
+++ app/src/main/java/com/example/fitmeal/RecommendMeals.java
@@ -5,15 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class RecommendMeals extends AppCompatActivity {
-
+    String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    Spinner spinner1;
+    TextView textView1;
+    ArrayAdapter<String> aa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_meals);
-
+        spinner1 = findViewById(R.id.spinner);
+        textView1 = findViewById(R.id.textView123);
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, days);
+        spinner1.setAdapter(aa);
+        spinner1.setOnItemSelectedListener(spinner1Listener);
     }
+    private AdapterView.OnItemSelectedListener spinner1Listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            textView1.setText(days[position]);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 
     public void OnClick(View view) {
 
