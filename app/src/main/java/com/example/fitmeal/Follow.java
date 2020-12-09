c85b394b5a60b0abef964ca2e7fca98506826a55
Maxim Hait <maxhait25@gmail.com>
2020-12-10 00:48:33 +0200

    Audio File

--- app/src/main/java/com/example/fitmeal/Follow.java
+++ app/src/main/java/com/example/fitmeal/Follow.java
@@ -3,15 +3,35 @@ package com.example.fitmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
-public class Follow extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
 
+public class Follow extends AppCompatActivity {
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_follow);
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        File path = android.os.Environment.getExternalStorageDirectory();
+        try {
+            mediaPlayer.setDataSource("C://Users//olgah//Downloads//NBA.mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void btnPlay(View view)
+    {
+        mp.start();
+    }
+    public void btnStop(View view)
+    {
+        mp.stop();
     }
     public void OnClick(View view) {
         switch (view.getId()) {
